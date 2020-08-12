067cd632d89dae99b3062fa3e7a74be783d80534
DigitalConfections <support@digitalconfections.com>
2020-08-11 21:15:54 -0400

    Ver 0.21
    o Corrects several errors that prevented the DIP switch from being read correctly after a sync.

--- Arduino-microfox/defs.h
+++ Arduino-microfox/defs.h
@@ -70,7 +70,7 @@
 
 /******************************************************
  * Set the text that gets displayed to the user */
-#define SW_REVISION "0.20"
+#define SW_REVISION "0.21"
 
 //#define TRANQUILIZE_WATCHDOG
 
